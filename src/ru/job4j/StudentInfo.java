4f36c617686aebe00260cc72cce45ff0fd0c7643
emelkin-pro <emelkin-pro@yandex.ru>
2021-06-01 21:29:14 +0300

    3.3. Разбор первой программы

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Oleg Emelyanov");
+        System.out.println("06.01.1994");
+    }
+}
