59d837b6c6f76cfe112ddca11e8257dc9cac80a7
emelkin-pro <emelkin-pro@yandex.ru>
2021-06-19 15:28:42 +0300

    4.1. Аргументы

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -4,10 +4,29 @@ public class ArgMethod {
     public static void hello(String name) {
         System.out.println("Hello, " + name);
     }
+    public static void hello() {
+        System.out.println("Hello, please enter your name and weight" );
+    }
+    public static void hello(String name, int age) {
+        System.out.println("Hello, " + name);
+        System.out.println("Your age: , " + age);
+    }
+    public static void hello(int age) {
+        System.out.println("Hello, your age: " + age);
+    }
+    public static void hello(String firstName, String secondName, String thirdName) {
+        System.out.println("Hello, " + firstName + " " + secondName + " " + thirdName);
+    }
     public static void main(String[] args) {
         String name = "Oleg Emelyanov";
         int age = 33;
 
-        ArgMethod.hello(name);
+        ArgMethod.hello();
+
+        ArgMethod.hello(name, age);
+
+        ArgMethod.hello(age);
+
+        ArgMethod.hello(name, name, name);
     }
 }
