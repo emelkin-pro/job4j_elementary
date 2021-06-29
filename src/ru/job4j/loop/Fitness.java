df5c641dfde6260ee83f808e1a2cde01c96111b3
emelkin-pro <emelkin-pro@yandex.ru>
2021-06-29 19:16:54 +0300

    5.4. Протеиновая диета

--- src/ru/job4j/loop/Fitness.java
+++ src/ru/job4j/loop/Fitness.java
@@ -3,11 +3,9 @@ package ru.job4j.loop;
 public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
-        int ivanPower = ivan;
-        int nikPower = nik;
-        while (ivanPower <= nikPower) {
-            ivanPower *= 3;
-            nikPower *= 2;
+        while (ivan <= nik) {
+            ivan *= 3;
+            nik *= 2;
             month++;
         }
         return month;
