e8b14e9723cf1c3ca5b1b38054de60e43b6cede9
emelkin-pro <emelkin-pro@yandex.ru>
2021-06-30 17:01:22 +0300

    6.2. Упорядочить массив.

--- src/ru/job4j/array/AlgoArray.java
+++ src/ru/job4j/array/AlgoArray.java
@@ -2,23 +2,19 @@ package ru.job4j.array;
 
 public class AlgoArray {
     public static void main(String[] args) {
-        int[] array = new int[]{5, 3, 2, 1, 4, 6, 9, 17, 12};
+        int[] array = new int[] {5, 3, 2, 1, 4};
         int temp = array[0]; /* переменная для временного хранения значение ячейки с индексом 0. */
         array[0] = array[3]; /* записываем в ячейку с индексом 0 значение ячейки с индексом 3. */
         array[3] = temp; /* записываем в ячейку с индексом 3 значение временной переменной. */
+        temp = array[1];
+        array[1] = array[2];
+        array[2] = temp;
+        temp = array[3];
+        array[3] = array[4];
+        array[4] = temp;
 
         for (int index = 0; index < array.length; index++) {
-            if (index + 1 != array.length) {
-                if (array[index] > array[index + 1]) {
-                    temp = array[index];
-                    array[index] = array[index + 1];
-                    array[index + 1] = temp;
-                    index = 0;
-                }
-            }
-        }
-        for (int cellArray: array) {
-            System.out.println(cellArray);
+            System.out.println(array[index]);
         }
     }
 }
