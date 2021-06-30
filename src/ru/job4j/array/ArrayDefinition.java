604b1e9b6e266121450bca5a305faf4cabd2383d
emelkin-pro <emelkin-pro@yandex.ru>
2021-06-30 13:31:05 +0300

    6.0.2. Заполнение массива.

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,8 +5,20 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+
+        names[0] = "Иван Михайлов";
+        names[1] = "Олег Емельянов";
+        names[2] = "Петр Зайцев";
+        names[3] = "Александра Лолкекова";
+
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println("Имя :" + names[i]);
+        }
+
     }
 }
