b8e9df72a82a30825d85d4773b00f414859fc0d0
emelkin-pro <emelkin-pro@yandex.ru>
2021-07-05 22:26:48 +0300

    6.8. Дефрагментация массива

--- src/ru/job4j/array/Defragment.java
+++ src/ru/job4j/array/Defragment.java
@@ -4,7 +4,7 @@ public class Defragment {
     public static String[] compress(String[] array) {
         for (int index = 0; index < array.length; index++) {
             if (array[index] == null) {
-                for (int j = index; j < array.length; j++) {
+                for (int j = index + 1; j < array.length; j++) {
                     if (array[j] != null) {
                         SwitchArray.swapString(array, index, j);
                         break;
